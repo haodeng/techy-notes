541675dfdb9072d69823e4f6afcdf0ade105180c
hao <hao@neoconsult.dk>
2018-12-16 22:28:20 +0100

    more optional test

--- src/test/java/optional/Java8OptionalTest.java
+++ src/test/java/optional/Java8OptionalTest.java
@@ -41,6 +41,26 @@ public class Java8OptionalTest {
                         ));
     }
 
+    @Test
+    public void test2(){
+        Car car = newCar2();
+
+        Assert.assertFalse(car.getWheels().get(2).get().getTire().isPresent());
+        Assert.assertFalse(car.getWheels().get(3).isPresent());
+
+        List<Tire> tires = car.getWheels().stream()
+                .map(wheel -> wheel.orElse(spareWheel()))
+                .map(wheel -> wheel.getTire().orElse(spareTire()))
+                .sorted((t1, t2) -> t1.getIdentifier().compareTo(t2.getIdentifier()))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals(4, tires.size());
+        Assert.assertEquals("Spare Tire", tires.get(0).getIdentifier());
+        Assert.assertEquals("Tire 1", tires.get(1).getIdentifier());
+        Assert.assertEquals("Tire 2", tires.get(2).getIdentifier());
+        Assert.assertEquals("Tire of spare wheel", tires.get(3).getIdentifier());
+    }
+
     private Car newCar(){
         Car car = new Car();
         List<Optional<Wheel>> wheels = new ArrayList<>();
@@ -64,4 +84,40 @@ public class Java8OptionalTest {
         car.setWheels(wheels);
         return car;
     }
+
+    private Car newCar2(){
+        Car car = new Car();
+        List<Optional<Wheel>> wheels = new ArrayList<>();
+
+        Optional<Wheel> wheel1 = Optional.of(new Wheel());
+        wheel1.get().setTire(Optional.of(new Tire("Tire 1")));
+        wheels.add(wheel1);
+
+        Optional<Wheel> wheel2 = Optional.of(new Wheel());
+        wheel2.get().setTire(Optional.of(new Tire("Tire 2")));
+        wheels.add(wheel2);
+
+        //No tire
+        Optional<Wheel> wheel3 = Optional.of(new Wheel());
+        wheel3.get().setTire(Optional.empty());
+        wheels.add(wheel3);
+
+        Optional<Wheel> wheel4 = Optional.empty();
+        wheels.add(wheel4);
+
+        car.setWheels(wheels);
+        return car;
+    }
+
+    private Wheel spareWheel(){
+        Wheel wheel = new Wheel();
+        wheel.setTire(Optional.of(new Tire("Tire of spare wheel")));
+
+        return wheel;
+    }
+
+    private Tire spareTire(){
+        return new Tire("Spare Tire");
+    }
+
 }
