07154e91e18d1dac0ab3be98f461156c3dddbbc5
haodeng <hao.deng.m@gmail.com>
2020-10-14 19:59:26 +0200

    Add MonthDayAndYearMonthDemo

---
+++ src/test/java/java8datetime/MonthDayAndYearMonthDemo.java
@@ -0,0 +1,29 @@
+package java8datetime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.MonthDay;
+import java.time.YearMonth;
+
+public class MonthDayAndYearMonthDemo {
+    @Test
+    public void now()
+    {
+        //--10-14
+        System.out.println(MonthDay.now());
+
+        //2020-10
+        System.out.println(YearMonth.now());
+    }
+
+    @Test
+    public void ofAndParse()
+    {
+        Assert.assertEquals("--10-20", MonthDay.of(10, 20).toString());
+        Assert.assertEquals("2020-10", YearMonth.of(2020, 10).toString());
+
+        Assert.assertEquals("--10-20", MonthDay.parse("--10-20").toString());
+        Assert.assertEquals("2020-10", YearMonth.parse("2020-10").toString());
+    }
+}
