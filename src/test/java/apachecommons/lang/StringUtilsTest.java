9ffb5a5ccd5cf943bcc2f61454cdc9e9e8a9b979
haodeng <hao.deng.m@gmail.com>
2020-11-08 21:11:33 +0100

    update StringUtilsTest

--- src/test/java/apachecommons/lang/StringUtilsTest.java
+++ src/test/java/apachecommons/lang/StringUtilsTest.java
@@ -70,6 +70,9 @@ public class StringUtilsTest {
 
         assertEquals("abcDEF", StringUtils.swapCase("ABCdef"));
         assertEquals("abcDEF  123!@#$?", StringUtils.swapCase("ABCdef  123!@#$?"));
+
+        assertEquals("Abc", StringUtils.capitalize("abc"));
+        assertEquals("ABC", StringUtils.capitalize("aBC"));
     }
 
     @Test
@@ -85,5 +88,135 @@ public class StringUtilsTest {
 
         assertEquals(2, StringUtils.indexOf("abaca", "a", 1));
         assertEquals(5, StringUtils.indexOf("abacaab", "ab", 1));
+
+        assertEquals(1, StringUtils.indexOfIgnoreCase("abaca", "B"));
+
+        assertEquals(0, StringUtils.indexOfAny("abc", "k", "a", "b"));
+        assertEquals(1, StringUtils.indexOfAny("abc", "j", "k", "b"));
+
+        assertEquals(4, StringUtils.lastIndexOf("abaca", "a"));
+        assertEquals(4, StringUtils.lastIndexOfIgnoreCase("abaca", "A"));
+
+        //Searches a CharSequence to find the first index of any character not in the given set of characters.
+        assertEquals(1, StringUtils.indexOfAnyBut("abaca", "a"));
+        assertEquals(3, StringUtils.indexOfAnyBut("abaca", "ab"));
+
+        //Compares all CharSequences in an array and returns the index at which the CharSequences begin to differ.
+        assertEquals(1, StringUtils.indexOfDifference("abaca", "a"));
+        assertEquals(0, StringUtils.indexOfDifference("abaca", "ba"));
+        assertEquals(3, StringUtils.indexOfDifference("abaca", "aba"));
+    }
+
+    @Test
+    public void test_equalCompare()
+    {
+        assertTrue(StringUtils.equals(null, null));
+        assertTrue(StringUtils.equals("", ""));
+        assertTrue(StringUtils.equals("ab", "ab"));
+        assertFalse(StringUtils.equals(null, "ab"));
+        assertFalse(StringUtils.equals("ab", null));
+        assertFalse(StringUtils.equals("aa", "ab"));
+
+        assertTrue(StringUtils.equalsIgnoreCase("ab", "ab"));
+        assertTrue(StringUtils.equalsIgnoreCase("ab", "Ab"));
+        assertTrue(StringUtils.equalsIgnoreCase("ab", "AB"));
+
+        assertTrue(StringUtils.equalsAny(null, null, ""));
+        assertTrue(StringUtils.equalsAny("ab", "ab", "ac"));
+        assertFalse(StringUtils.equalsAny("ab", "aa", "ac"));
+
+        assertTrue(StringUtils.equalsAnyIgnoreCase("ab", "AB", "ac"));
+
+        assertEquals(0, StringUtils.compare(null, null));
+        assertEquals(0, StringUtils.compare("a", "a"));
+        assertEquals(1, StringUtils.compare("a", null));
+        assertEquals(-1, StringUtils.compare("a", "b"));
+        assertEquals(1, StringUtils.compare("b", "a"));
+
+        //null is greater/less
+        assertEquals(0, StringUtils.compare("a", "a", false));
+        assertEquals(1, StringUtils.compare("a", null, true));
+        assertEquals(-1, StringUtils.compare("a", null, false));
+
+        assertEquals(0, StringUtils.compareIgnoreCase("a", "A"));
+        assertEquals(0, StringUtils.compareIgnoreCase("a", "a"));
+    }
+
+    @Test
+    public void test_length()
+    {
+        assertEquals(0, StringUtils.length(null));
+        assertEquals(0, StringUtils.length(""));
+        assertEquals(1, StringUtils.length("1"));
+    }
+
+    @Test
+    public void test_trim()
+    {
+        assertEquals(null, StringUtils.trim(null));
+        assertEquals("", StringUtils.trim(""));
+        assertEquals("", StringUtils.trim("  "));
+        assertEquals("a", StringUtils.trim("a "));
+        assertEquals("a", StringUtils.trim("  a"));
+        assertEquals("a", StringUtils.trim(" a  "));
+        assertEquals("a b", StringUtils.trim(" a b  "));
+
+        assertEquals("", StringUtils.trimToEmpty(null));
+        assertEquals("", StringUtils.trimToEmpty(""));
+
+        assertEquals(null, StringUtils.trimToNull(null));
+        assertEquals(null, StringUtils.trimToNull(""));
+        assertEquals(null, StringUtils.trimToNull("   "));
+        assertEquals("a", StringUtils.trimToNull("  a  "));
+    }
+
+    @Test
+    public void test_pad()
+    {
+        assertEquals(null, StringUtils.leftPad(null, 3));
+        assertEquals("   ", StringUtils.leftPad("", 3));
+        assertEquals("  a", StringUtils.leftPad("a", 3));
+        assertEquals(" aa", StringUtils.leftPad("aa", 3));
+        assertEquals("aaa", StringUtils.leftPad("aaa", 3));
+
+        assertEquals("!!!", StringUtils.leftPad("", 3, "!"));
+
+        assertEquals(null, StringUtils.rightPad(null, 3));
+        assertEquals("   ", StringUtils.rightPad("", 3));
+        assertEquals("a  ", StringUtils.rightPad("a", 3));
+        assertEquals("aa ", StringUtils.rightPad("aa", 3));
+        assertEquals("aaa", StringUtils.rightPad("aaa", 3));
+
+        assertEquals("!!!", StringUtils.rightPad("", 3, "!"));
+    }
+
+    @Test
+    public void test_abbreviate()
+    {
+        //Abbreviates a String using ellipses.
+        assertEquals("Hi, this is...", StringUtils.abbreviate("Hi, this is abc", 14));
+    }
+
+    @Test
+    public void test_leftRightMidCenter()
+    {
+        assertEquals(null, StringUtils.left(null, 3));
+        assertEquals("", StringUtils.left("abcdefg", -1));
+        assertEquals("", StringUtils.left("abcdefg", 0));
+        assertEquals("abc", StringUtils.left("abcdefg", 3));
+        assertEquals("abcdefg", StringUtils.left("abcdefg", 7));
+        assertEquals("abcdefg", StringUtils.left("abcdefg", 100));
+
+        assertEquals(null, StringUtils.right(null, 3));
+        assertEquals("", StringUtils.right("abcdefg", -1));
+        assertEquals("", StringUtils.right("abcdefg", 0));
+        assertEquals("efg", StringUtils.right("abcdefg", 3));
+        assertEquals("abcdefg", StringUtils.right("abcdefg", 7));
+        assertEquals("abcdefg", StringUtils.right("abcdefg", 100));
+
+        assertEquals("bcd", StringUtils.mid("abcdefg", 1, 3));
+        assertEquals("cde", StringUtils.mid("abcdefg", 2, 3));
+
+        assertEquals(" ab ", StringUtils.center("ab", 4));
     }
 }
