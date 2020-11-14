8a9028a31ee7c3b61e11fe1c3cab0ae99c741168
haodeng <hao.deng.m@gmail.com>
2020-11-14 23:25:35 +0100

    move optional to java8

--- src/test/java/java8/optional/OptionalTest.java
+++ src/test/java/java8/optional/OptionalTest.java
@@ -1,4 +1,4 @@
-package optional;
+package java8.optional;
 
 import lombok.bean.Car;
 import lombok.bean.Tire;
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class Java8OptionalTest {
+public class OptionalTest {
 
     @Test
     public void test(){
