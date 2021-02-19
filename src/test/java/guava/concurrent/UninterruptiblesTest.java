fb6ebc6e0bd5c81d96bc6bb92263e3170e499ade
haodeng <hao.deng.m@gmail.com>
2021-02-19 10:58:21 +0100

    Add UninterruptiblesTest

--- src/test/java/guava/concurrent/UninterruptiblesTest.java
+++ src/test/java/guava/concurrent/UninterruptiblesTest.java
@@ -1,2 +1,22 @@
-package guava.concurrent;public class UninterruptiblesTest {
+package guava.concurrent;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class UninterruptiblesTest {
+
+    @Test
+    public void sleep()
+    {
+        long before = System.currentTimeMillis();
+        // a handy way to avoid dealing with the InterruptedExceptions is using Guava's Uninterruptibles
+        Uninterruptibles.sleepUninterruptibly(1000, TimeUnit.MILLISECONDS);
+
+        long after = System.currentTimeMillis();
+
+        Assert.assertTrue(after - 1000 >= before);
+    }
 }
