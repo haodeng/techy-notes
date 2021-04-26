b7c90dc7162ad237f187063501a939c15f2cace8
haodeng <hao.deng.m@gmail.com>
2021-04-27 01:05:39 +0200

    add ThreadRunInSequence

---
+++ src/main/java/concurrent/ThreadRunInSequence.java
@@ -0,0 +1,34 @@
+package concurrent;
+
+public class ThreadRunInSequence {
+    private static Object lock = new Object();
+
+    static class ThreadA implements Runnable {
+        @Override
+        public void run() {
+            synchronized (lock) {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("Thread A " + i);
+                }
+            }
+        }
+    }
+
+    static class ThreadB implements Runnable {
+        @Override
+        public void run() {
+            // when thread A locks, thread B has to wait A done and release the lock
+            synchronized (lock) {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("Thread B " + i);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // After A is done, B start executing
+        new Thread(new ThreadA()).start();
+        new Thread(new ThreadB()).start();
+    }
+}
