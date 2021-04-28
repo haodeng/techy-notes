f9f0e79e353afe9148cf942857cb78ce3468881e
haodeng <hao.deng.m@gmail.com>
2021-04-28 20:24:16 +0200

    Update ThreadsBlockTest

--- src/main/java/concurrent/ThreadsBlockTest.java
+++ src/main/java/concurrent/ThreadsBlockTest.java
@@ -18,14 +18,15 @@ public class ThreadsBlockTest {
         @SneakyThrows
         @Override
         public void run() {
-
+            // lock
             synchronized (ThreadsBlockTest.class) {
                 System.out.println(this.runId);
                 // Thread 4 blocks all other threads
                 if (runId == 4) {
                     // A time-consuming operation
-                    // bad practice!
-                    Thread.sleep(500 * 1000);
+                    // bad practice to use sleep while holding the lock!
+                    // should use wait() instead
+                    Thread.sleep(5 * 1000);
                 }
                 else {
                     Thread.sleep(1 * 1000);
@@ -33,6 +34,32 @@ public class ThreadsBlockTest {
 
                 System.out.println("complete " + runId);
             }
+        }
+    }
+
+    static class TestRunNoLock implements Runnable
+    {
+        private int runId;
+        public TestRunNoLock(int runId)
+        {
+            this.runId = runId;
+        }
+
+        @SneakyThrows
+        @Override
+        public void run() {
+
+            System.out.println(this.runId);
+            if (runId == 4) {
+                // A time-consuming operation
+                // But only blocks current thread
+                Thread.sleep(5 * 1000);
+            }
+            else {
+                Thread.sleep(1 * 1000);
+            }
+
+            System.out.println("complete " + runId);
         }
     }
 
@@ -68,6 +95,7 @@ public class ThreadsBlockTest {
         System.out.println("complete " + runId);
     }
 
+
     class TestRunWithWait implements Runnable
     {
         private int runId;
@@ -88,12 +116,14 @@ public class ThreadsBlockTest {
     // sleep only release CPU, do NOT release lock. May get deadlock
     @SneakyThrows
     private void blockTestWithWait(int runId) {
+        // lock
         synchronized (lock) {
             System.out.println(runId);
 
             // Thread 4 NOT blocks other threads
             if (runId == 4) {
                 // A time-consuming operation
+                // wait release the lock, so other thread can enter
                 lock.wait(10 * 1000);
             }
             else {
@@ -118,8 +148,11 @@ public class ThreadsBlockTest {
 //        IntStream.range(1, 6)
 //                .forEach(i -> new Thread(new TestRun(i)).start());
 
+//        IntStream.range(1, 6)
+//                .forEach(i -> new Thread(new TestRunNoLock(i)).start());
+
         ThreadsBlockTest test = new ThreadsBlockTest();
-//        test.runTest();
+        test.runTest();
 
         test.runTestWithWait();
     }
